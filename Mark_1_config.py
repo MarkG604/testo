3e2736f8afa8a0cc6c783160f2e5069bc0590896
MarkG604 <155362041+MarkG604@users.noreply.github.com>
2024-01-01 14:55:35 +0300

    Add files via upload

---
+++ Mark_1_config.py
@@ -0,0 +1,40 @@
+
+# BOT_TOKEN ='6831690029:AAFaIDKUw5-Swvx5A2QowSaT2Z1sIp49uek'
+BOT_TOKEN = '6974338167:AAEqiox57ZRmzmEPbwtXLxImuheMCtnKys4'
+admin_id = 1505389685
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import requests
+#
+# API_link = 'https://api.telegram.org/bot6974338167:AAEqiox57ZRmzmEPbwtXLxImuheMCtnKys4'
+#
+# updates = requests.get(API_link + '/getUpdates?offset=-1').json()
+#
+#
+# print(updates)
+#
+# message = updates['result'][0]['message']
+#
+# chat_id = message['from']['id']
+# text = message['text']
+#
+# sent_message = requests.get(API_link + f'/sendMessage?chat_id={chat_id}&text=Hello, you wrote {text}')
